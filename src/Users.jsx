31d83dc4f748eefdc389dff2a3f3ee60814d2556
rootnure <nur.diu.2791@gmail.com>
2023-10-14 11:27:32 +0600

    Made delete btn functional using DELETE method

--- src/Users.jsx
+++ src/Users.jsx
@@ -7,6 +7,19 @@ const Users = () => {
 
     const handleDelete = _id => {
         console.log('delete', _id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    alert('Deleted');
+                }
+                else {
+                    alert('Failed to delete');
+                }
+            })
     }
 
     return (
